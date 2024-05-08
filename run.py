50c345d4037894248d84f1a8274076edf4144c67
f3s123 <ksuyeon0124@gmail.com>
2024-05-08 18:22:52 +0900

    modify login&main&run

--- run.py
+++ run.py
@@ -3,14 +3,24 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def login():
-    return render_template('login.html', methods=['POST'])
+def login_page():
+    return render_template('login.html')
 
-@app.route('/main', methods=['POST'])
-def main():
-    print(request.form.get('id'))
-    print(request.form.get('pw'))
+@app.route('/login', methods=['POST'])
+def login():
     return render_template('main.html')
+
+@app.route('/register')
+def register():
+    return render_template('login.html')
+
+@app.route('/diary_board')
+def diary_board():
+    return render_template('diary_board.html')
+
+@app.route('/logout')
+def logout():
+    return login_page()
     
 if __name__ == '__main__':
     app.run('127.0.0.1', 5000, debug=True)
